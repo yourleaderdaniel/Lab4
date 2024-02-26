f052d4c9290181a7a61e6c09d0b27ce226988e93
yourleaderdaniel <96195035+yourleaderdaniel@users.noreply.github.com>
2024-02-26 23:29:35 +0200

    Add files via upload

---
+++ YourAspNetCoreProject/Controllers/LibraryController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+[Route("[controller]")]
+public class LibraryController : Controller
+{
+    private IConfiguration _configuration;
+
+    public LibraryController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return Content("Greetings fellow book enjoyer!!!!");
+    }
+
+    [HttpGet("Books")]
+    public IActionResult Books()
+    {
+        var booksConfig = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("books.json")
+            .Build();
+
+        var books = booksConfig.GetSection("Books").Get<List<string>>();
+        return Content(string.Join("\n", books)); 
+    }
+
+
+    [HttpGet("Profile/{id?}")]
+    public IActionResult Profile(int? id)
+    {
+        
+        var userId = id.GetValueOrDefault(0); // 0 это id профиля по умолчанию
+
+        var profilesConfig = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("profiles.json")
+            .Build();
+
+        
+        var profile = profilesConfig.GetSection($"Profiles:{userId}").Get<string>();
+        return Content(string.IsNullOrEmpty(profile) ? "Profile not found" : profile);
+    }
+
+
+}
---
+++ YourAspNetCoreProject/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MyAspNetCoreProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews(); 
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage(); 
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error"); 
+                app.UseHsts(); 
+            }
+
+            app.UseHttpsRedirection(); 
+            app.UseStaticFiles(); 
+
+            app.UseRouting(); 
+
+            app.UseAuthorization(); 
+
+            app.UseEndpoints(endpoints =>
+            {
+                
+                endpoints.MapControllerRoute(
+                    name: "library",
+                    pattern: "Library",
+                    defaults: new { controller = "Library", action = "Index" });
+
+                endpoints.MapControllerRoute(
+                    name: "books",
+                    pattern: "Library/Books",
+                    defaults: new { controller = "Library", action = "Books" });
+
+                endpoints.MapControllerRoute(
+                    name: "profile",
+                    pattern: "Library/Profile/{id?}",
+                    defaults: new { controller = "Library", action = "Profile" },
+                    constraints: new { id = @"\d{0,5}" }); 
+            });
+        }
+    }
+}
